047196c7546c1147a6fa2939a10e667eff8bed17
ThisIsMatthew <thisismatt@me.com>
2023-12-02 21:52:35 +1100

    scene tweaks and imported TMP

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject HandBone;
+    // Update is called once per frame
+
+
+}
